a77207dbf7e9d8e5632245234ec987c095215b95
Abhinav Palugula <abhinav.palugula@gmail.com>
2023-08-27 09:11:41 +0530

    Initial Release

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,30 +3,7 @@ let express = require("express")
 let router = express.Router()
 
 router.route("/").get((req, res) => {
-    const options = {
-        method: 'GET',
-        hostname: 'online-movie-database.p.rapidapi.com',
-        port: null,
-        path: '/title/v2/get-popular-movies-by-genre?limit=100',
-        headers: {
-            'X-RapidAPI-Key': '9287e9b0e5mshe0ec66817e6f3a5p15f6b9jsnd02d3f73d726',
-            'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-        }
-    };
-
-    const request = http.request(options, function (res) {
-        let resData = "";
-
-        res.on('data', function (chunk) {
-            resData += chunk
-        });
-
-        res.on('end', function () {
-            console.log(JSON.parse(resData));
-        });
-    });
-
-    request.end();
+    res.json({ key1: "Hello" })
 })
 
 module.exports = router
