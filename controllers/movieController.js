84044b042b7bcd08daf23a2cadf008b08d542d85
Abhinav Palugula <abhinav.palugula@gmail.com>
2023-08-29 22:50:16 +0530

    Responsive Design

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,6 +2,7 @@ let http = require("https")
 let express = require("express")
 let router = express.Router()
 require("dotenv").config()
+router.use(express.urlencoded({ extended: true }))
 router.route("/").get((req, res) => {
 
     const options = {
@@ -27,6 +28,35 @@ router.route("/").get((req, res) => {
         });
     });
     request.end();
+})
+
+router.get("/movie/:id", (req, res) => {
+    let movieID = req.params.id;
+    const options = {
+        method: 'GET',
+        hostname: 'api.themoviedb.org',
+        port: null,
+        path: `/3/movie/${movieID}?language=en-US`,
+        headers: {
+            accept: 'application/json',
+            Authorization: process.env.Authorization
+        }
+    };
+
+    const request = http.request(options, function (response) {
+        let resData = ""
+
+        response.on('data', function (chunk) {
+            resData += chunk
+        });
+
+        response.on('end', function () {
+            const resJSON = JSON.parse(resData);
+            res.json(resJSON)
+        });
+    });
+
+    request.end()
 })
 
 module.exports = router
